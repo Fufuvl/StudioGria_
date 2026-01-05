c3dfbdb133936fbb806c3ee70140c4bdac854846
SelenArikan <20comp1036@isik.edu.tr>
2026-01-05 22:36:46 +0300

    Google Analytics (gtag.js) eklendi - G-4EWVJ0Y6EC tracking ID ile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import {
   Syne,
   Aladin,
@@ -78,6 +79,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-4EWVJ0Y6EC"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4EWVJ0Y6EC');
+            `,
+          }}
+        />
+      </head>
       <body
         id="body"
         suppressHydrationWarning={true}
